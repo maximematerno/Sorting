2fe1843e0d70b1f49e4b845d8e358d7bb479f978
Maxime Vacher-Materno <37785179+maximematerno@users.noreply.github.com>
2020-02-19 23:55:36 -0800

    Update searching.py

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,30 +1,38 @@
 # STRETCH: implement Linear Search				
 def linear_search(arr, target):
-  
-  # TO-DO: add missing code
-
-   return -1   # not found
+  for i in arr:
+    if i == target:
+      return True
+   return False
 
 
 # STRETCH: write an iterative implementation of Binary Search 
 def binary_search(arr, target):
 
-  if len(arr) == 0:
-    return -1 # array empty
-    
-  low = 0
-  high = len(arr)-1
-
-  # TO-DO: add missing code
-
-  return -1 # not found
 
 
 # STRETCH: write a recursive implementation of Binary Search 
 def binary_search_recursive(arr, target, low, high):
-  
-  middle = (low+high)//2
-
-  if len(arr) == 0:
-    return -1 # array empty
-  # TO-DO: add missing if/else statements, recursive calls
+  start = 0
+  end = len(arr)
+  while end - start > 0:
+    mid = start + (end - start) // 2
+    item = arr[mid]
+    print(start)
+    print(mid)
+    print(end)
+    print('***')
+    
+    # If the item is equal to arr
+    
+    if item == target:
+      return True
+    
+    # if its smaller
+    
+    elif item > target:
+      end = mid - 1
+      
+    else:
+      start = mid + 1
+  return False 
